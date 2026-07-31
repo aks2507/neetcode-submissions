0f858aa0f380024dedfd8a941dce204d9cf73c25
Abhishek Kumar Shukla <as841840@student.nitw.ac.in>
2026-08-01 01:01:01 +0530

    Add: number-of-senior-citizens - submission-0

---
+++ Data Structures & Algorithms/number-of-senior-citizens/submission-0.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int countSeniors(vector<string>& details) {
+        int count = 0;
+        for (string s : details) {
+            count += stoi(s.substr(11, 2)) > 60;
+        }
+
+        return count;
+    }
+};
