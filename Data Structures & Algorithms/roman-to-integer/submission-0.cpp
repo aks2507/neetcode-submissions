6d2bcb25cbd14adc55a31cd921195bb29522e55f
Abhishek Kumar Shukla <as841840@student.nitw.ac.in>
2026-08-31 19:46:28 +0530

    Add: roman-to-integer - submission-0

---
+++ Data Structures & Algorithms/roman-to-integer/submission-0.cpp
@@ -0,0 +1,24 @@
+class Solution {
+    unordered_map<char, int> mp = {
+        {'I', 1},
+        {'V', 5},
+        {'X', 10},
+        {'L', 50},
+        {'C', 100},
+        {'D', 500},
+        {'M', 1000}
+    };
+public:
+    int romanToInt(string s) {
+        int res = 0, n = s.length();
+        for (int i = 0; i < n; i++) {
+            if (i + 1 < n && mp[s[i]] < mp[s[i + 1]]) {
+                res -= mp[s[i]];
+            } else {
+                res += mp[s[i]];
+            }
+        }
+
+        return res;
+    }
+};
